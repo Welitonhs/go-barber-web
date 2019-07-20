1d40653ff0cfa4910a1c712008f318fe54102282
welitonsernajotto <weliton.sernajotto23@gmail.com>
2019-07-20 16:42:34 -0300

    Adicionado redux, redux-saga, adicionada api para autenticação

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,24 +1,27 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
+import { signInRequest } from '~/store/modules/auth/actions';
+
 import logo from '~/assets/logo.svg';
 
-// import { Container } from './styles';
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido.')
+    .required('E-mail é obrigatório.'),
+  password: Yup.string()
+    .min(6)
+    .required('Senha é obrigatória.'),
+});
 
 export default function SignIn() {
-  const schema = Yup.object().shape({
-    email: Yup.string()
-      .email('Insira um e-mail válido.')
-      .required('E-mail é obrigatório.'),
-    password: Yup.string()
-      .min(8)
-      .required('Senha é obrigatória.'),
-  });
+  const dispatch = useDispatch();
 
-  function handleSubmit(data) {
-    console.tron.log(data);
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
   }
 
   return (
