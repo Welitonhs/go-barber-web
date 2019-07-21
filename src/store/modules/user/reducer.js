d122eba70a305414fcda0c7cf7050604bdf9f15d
welitonsernajotto <weliton.sernajotto23@gmail.com>
2019-07-21 20:51:06 -0300

    Adicionando loading na tela de login

--- src/store/modules/user/reducer.js
+++ src/store/modules/user/reducer.js
@@ -6,6 +6,10 @@ const INITIAL_STATE = {
 
 export default function user(state = { INITIAL_STATE }, action) {
   switch (action.type) {
+    case '@auth/SIGN_IN_REQUEST':
+      return produce(state, draft => {
+        draft.profile = action.payload.user;
+      });
     case '@auth/SIGN_IN_SUCCESS':
       return produce(state, draft => {
         draft.profile = action.payload.user;
